8ddfb53abec986b557fcd2daeef5a07f259a8624
emanuell14 <128757177+emanuell14@users.noreply.github.com>
2025-05-08 11:36:15 -0300

    Add files via upload

---
+++ questão1 de listas.py
@@ -0,0 +1,10 @@
+#area variables (in square mters)
+hall = 11.25
+kit = 18
+liv = 28
+bed = 10.75
+bath = 9.5
+#adapt list areas
+areas = ["hallway",hall,"kitchen", kit,"living room", liv,"bedroom", bed,"bathroom", bath]
+#print areas
+print(areas)
