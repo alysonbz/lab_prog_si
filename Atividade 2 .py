8d195e8de5f669c43a52764fe929e404ccd3a73e
JoaoPedro2324 <joaopedroalmeidaandre784@gmail.com>
2025-05-15 11:09:59 -0300

    Update Atividade 2 .py

--- Atividade 2 .py
+++ Atividade 2 .py
@@ -6,6 +6,7 @@ bed = 10.75
 bath = 9.5
 areas = ["Hallway",hall,"Kitchen", kit, "Livingroom", liv, "Bedroom", bed,"Bathroom",   bath]
 print(areas)
+
 #Questão 2
 listaA = [1,3,4,2]
 print(listaA)
@@ -14,6 +15,7 @@ print(listaB)
 listaC = [1+2, "a" * 5, 3]
 print(listaC)
 #Todas as listas estão corretas; Listas comuns, e listas dentro de listas
+
 #Questão 3
 hall = 11.25
 kit = 18.0
@@ -27,29 +29,35 @@ house = [["hallway", hall],
          ["bathroom", bath]]
 print(house)
 print(type(house))
+
 #Questão 4
 areas = ["hallway", 11.25, "kitchen", 18.0, "Living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 print(areas[1])
 print(areas[-1])
 print(areas[5])
+
 #Questão 5
 areas = ["hallway", 11.25, "kitchen", 18.0, "Living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 eat_sleep_area = areas[3] + areas[5]
 print(eat_sleep_area)
+
 #Questão 6
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 downstairs = areas[:6]
 upstairs = areas[-4:]
 print("Downstairs:", downstairs)
 print("Upstairs:", upstairs)
+
 #Questão abcd
 #c) A float: the bathroom area
+
 #Questão 7
 areas = ["hallway", 11.25,
          "kitchen", 18.0, "Chill Zone",
          20.0, "bedroom", 10.75,
          "bathroom", 10.50]
 print(areas)
+
 #Questão 8
 areas = ["hallway", 11.25, "kitchen", 18.0,
          "chill zone", 20.0,
@@ -57,6 +65,7 @@ areas = ["hallway", 11.25, "kitchen", 18.0,
 areas_1 = areas + ["poolhouse", 24.5]
 areas_2 = areas_1 + ["garage", 15.5]
 print(areas_2)
+
 #Questão 9
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 areas_copy = areas.copy()  # Alternativamente, areas[:] também funciona
