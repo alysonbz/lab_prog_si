5e54868a0bfc0357d3645e0f0669e4aaed0fc40f
kauan23-cmd <kauanlimaparacuru@alu.ufc.br>
2025-05-29 10:40:13 -0300

    Add files via upload

---
+++ aula23_05.py
@@ -0,0 +1,19 @@
+#questão 1
+var1= [1, 2, 3, 4]
+var2 = True
+print(type(var1))
+#questão 2
+print(len(var1))
+#questão 3
+print(int(var2))
+#questão 4
+first=[11.25, 18.0, 20.0]
+second=[10.75, 9.50]
+full=first+second
+print(full)
+#questão 5
+sorted(full)
+full_sorted=sorted(full)
+reverse=True
+#questão 6
+print(full_sorted)
